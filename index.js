c71667a0b27383a63ec0f4bffe7d836037538dd7
TheDomesticUser <TheDomesticUser@gmail.com>
2019-12-15 14:54:03 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// const key = 'code';
+// const url = document.URL;
+
+// let code = decodeURI(url.slice(url.indexOf(key) + key.length + 1));
+
+// // Get the code separated by the {} brackets
+// code = code.split('{').pop().split('}')[0];
+
+// eval(code);
+
+alert(1);
